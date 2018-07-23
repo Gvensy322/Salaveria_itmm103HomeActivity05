a986496ceae9a09ab047a4c65a371cf36d58a318
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-07-23 08:24:37 +0800

    Create compute.php

---
+++ compute.php
@@ -0,0 +1,78 @@
+<?php
+		for ($i=1; $i<=31; $i++)
+		{
+		$arDays[]=$i;
+	}
+	echo '<select name="day">';
+		foreach($arDays as $option){
+
+		echo'<option value="'.$option.'">'.$option.'</option>';
+	}
+echo '</select>';
+?>
+
+
+<select name="month">
+	<option value="01">Jan</option>
+	<option value="02">Feb</option>
+	<option value="03">Mar</option>
+	<option value="04">Apr</option>
+	<option value="05">May</option>
+	<option value="06">Jun</option>
+	<option value="07">Jul</option>
+	<option value="08">Aug</option>
+	<option value="09">Sep</option>
+	<option value="10">Oct</option>
+	<option value="11">Nov</option>
+	<option value="12">Dec</option>
+</select>
+
+<?php
+$currentYear = date("Y");
+	for($i=$currentYear; $i >=1920; $i--)
+	{
+		$arYears[]= $i;
+
+}
+echo '<select name="year">';
+	foreach($arYears as $option){
+	echo '<option value"'.$option.'">'.$option.'</option>';
+
+}
+echo '</select>';
+?>
+<form name="frm" method="post" action="display.php">
+<input type="submit" name="submit" value="Calculate"/>
+
+<?php
+ if(isset($_POST['submit'])){
+$day=$_POST['day'];
+$month=$_POST['month'];
+$year=$_POST['year'];
+$birthDate= $year.'-'.$month.'-'.$day;
+
+$dob = new DateTime($birthDate);
+
+$interval = $dob->diff(new DateTime);
+echo "<br />";
+echo "Date of Birth (yyyy-mm-dd): ".$birthDate;
+echo "<br />";
+echo "Your Age: ".$interval->y;
+
+
+	
+
+
+
+
+
+}
+
+
+
+
+
+
+
+?>
+</form>
