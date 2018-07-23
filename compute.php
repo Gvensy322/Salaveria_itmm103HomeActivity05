9cfaa4a4847d32fe2a623b956f42008b31dcfbc4
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-07-23 08:39:36 +0800

    Update compute.php

--- compute.php
+++ compute.php
@@ -41,29 +41,10 @@ echo '<select name="year">';
 }
 echo '</select>';
 ?>
-<form name="frm" method="post" action="">
+<form name="frm" method="post" action="display.php">
 <input type="submit" name="submit" value="Calculate"/>
 
 	
-<?php
- if(isset($_POST['submit'])){
-$day=$_POST['day'];
-$month=$_POST['month'];
-$year=$_POST['year'];
-$birthDate= $year.'-'.$month.'-'.$day;
 
-$dob = new DateTime($birthDate);
-
-$interval = $dob->diff(new DateTime);
-echo "<br />";
-echo "Date of Birth (yyyy-mm-dd): ".$birthDate;
-echo "<br />";
-echo "Your Age: ".$interval->y;
-
-
-}
-
-
-?>
 </form>
 
