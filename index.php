e221074fd079091d55e2700c30d4a3fa8934a743
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-07-23 08:23:35 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<title>Calculate Age</title>
+</head>
+	
+<body>
+	<h3>Calculate Age from Date of Birth Using PHP</h3>
+	<form name="frm" method="post" action="compute.php">
+	<input type="submit" name="submit" value="Click Here"/>
+</form>
+		
+
+
+
+	
+
+		
+</body>
+</html>
