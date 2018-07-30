88303d96191e2ec7a9175ad04c5eda14556949d6
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-07-30 15:06:30 +0800

    Create display.php

---
+++ display.php
@@ -0,0 +1,14 @@
+<?php
+ if(isset($_POST['submit'])){
+$day=$_POST['day'];
+$month=$_POST['month'];
+$year=$_POST['year'];
+$birthDate= $year.'-'.$month.'-'.$day;
+$dob = new DateTime($birthDate);
+$interval = $dob->diff(new DateTime);
+echo "<br />";
+echo "Date of Birth (yyyy-mm-dd): ".$birthDate;
+echo "<br />";
+echo "Your Age: ".$interval->y;
+}
+?>
