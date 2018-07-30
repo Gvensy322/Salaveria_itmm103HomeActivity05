0a912f86d3c7219fa9fa9fb05af442b99508f18c
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-07-30 14:56:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 </head>
 	
 <body>
+<form action = "compute.php" method="post">
 	<h3>Calculate Age from Date of Birth Using PHP</h3>
 	<form name="frm" method="post" action="compute.php">
 	<input type="submit" name="submit" value="Click Here"/>
