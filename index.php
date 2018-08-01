63cbc765da847f7d1ea323a7398a29ec00fd3245
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-08-02 07:16:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 <html>
 <head>
 	
-<title>Calculate Age Activity5a</title>
+<title>Calculate Age</title>
 </head>
 	
 <body>
 <form action = "compute.php" method="post">
-	<h3>Calculate Age from Date of Birth Using PHP</h3>
+	
+	<h>Activity 5a</h>
+	<h1>Calculate Age from Date of Birth Using PHP</h1>
 	
 
 
 <input type="submit" name="submit" value="Click Here"/>
 	
+	<h>Activity 5b </h>
 	<a href=Activity5b.php>Link to Activity 5b</a>
 	
 	
