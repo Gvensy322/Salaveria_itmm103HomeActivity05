45952a1598f03e5a593bd4ac853d00495405c4cf
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-08-02 07:09:25 +0800

    Create Activity5b.php

---
+++ Activity5b.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+  <h>Activity 5b.1</h>
+  <br>
+  <br>
+
+<?php
+echo readfile("HelloWorld.txt");
+?>
+  <br>
+  <br>
+
+  
+  <h>Activity 5b.2</h>
+  <br>
+  <br>
+  
+ <?php 
+for ($x = 0; $x <= 10; $x++) {
+    echo "Hello World <br>";
+} 
+  
+?>
+  <br>
+  <br>
+    <h>Activity 5b.3</h>
+  <br>
+  <br>
+
+<?php
+echo readfile("NewHelloWorld.txt");
+?>
+  
+  
+  
+
+</body>
+</html>
